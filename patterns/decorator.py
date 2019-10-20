6c5096e11f63aa1250d0d9b801585019a149c075
Matěj Beran <matej.beran96@gmail.com>
2019-10-20 18:15:54 +0200

    Trying the python decorator

---
+++ patterns/decorator.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+@author Matěj Beran
+"""
+
+
+def currency(func):
+    def func_wrapper(*args, **kwargs):
+        return str(func(*args, **kwargs)) + " Kč"
+
+    return func_wrapper
+
+
+@currency
+def add(a, b):
+    return a + b
+
+
+def visual_test():
+    print("{0}".format(add(10, 10)))
+
+
+if __name__ == '__main__':
+    visual_test()
