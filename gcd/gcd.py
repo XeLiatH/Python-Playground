08509d2fd414f4e1e1fade77bcaeed1708d1c2e4
Matěj Beran <matej.beran96@gmail.com>
2019-10-20 14:34:00 +0200

    Add greatest common divisor using Euclid's algorithm

---
+++ gcd/gcd.py
@@ -0,0 +1,65 @@
+"""
+
+@author Matěj Beran
+
+Greatest Common Divisor (GCD)
+---
+Using the Euclid's algorithm
+Can be called from command line
+
+"""
+
+import sys
+import argparse
+
+
+def gcd(a: int, b: int):
+    r = a % b
+    if r == 0:
+        return b
+    return gcd(b, r)
+
+
+def gcd_multiple(numbers: list):
+    num_len = len(numbers)
+    # TODO: Ugh, don't think thi is cool
+    if num_len == 1:
+        return numbers[0]
+
+    if num_len == 2:
+        return gcd(numbers[0], numbers[1])
+
+    _gcd = 1
+    for i in range(num_len - 2):
+        _gcd = gcd(numbers[i], gcd(numbers[i + 1], numbers[i + 2]))
+    return _gcd
+
+
+def parse_args(args):
+    """
+    Parses command line arguments
+    """
+
+    parser = argparse.ArgumentParser(
+        description="Computes the greatest common divisor using the Euclidean algorithm")
+
+    # TODO: would like to constraint nargs to 2 or more
+    parser.add_argument('numbers', type=int, nargs='+',
+                        help="Numbers to determine greates common divisor on")
+
+    return parser.parse_args(args)
+
+
+def handle_io(args):
+    """
+    Handles the input arguments of the program
+    """
+
+    parsed = parse_args(args)
+    print(gcd_multiple(parsed.numbers))
+
+    return 0
+
+
+if __name__ == "__main__":
+    handle_io(sys.argv[1:])
---
+++ gcd/gcd_test.py
@@ -0,0 +1,32 @@
+"""
+
+@author Matěj Beran
+
+Greatest Common Divisor (GCD)
+---
+Using the Euclid's algorithm
+
+"""
+
+import gcd
+
+
+def test_default():
+    assert gcd.gcd(10, 10) == 10
+
+
+def test_same():
+    assert gcd.gcd(10, 5) == gcd.gcd(10, 5)
+
+
+def test_constant():
+    c = 5
+    assert c * gcd.gcd(10, 5) == gcd.gcd(c * 10, c * 5)
+
+
+def test_impersonal():
+    assert gcd.gcd(7, 11) == 1
+
+
+def test_multiple():
+    assert gcd.gcd_multiple([10, 20, 30]) == 10
